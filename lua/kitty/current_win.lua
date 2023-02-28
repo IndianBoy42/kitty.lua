df0314d37d53e3b48ea34a1e04f01d932743a51f
Anshuman <amedhi@connect.ust.hk>
2023-03-01 00:04:14 +0800

    Initial Commit

---
+++ lua/kitty/current_win.lua
@@ -0,0 +1,28 @@
+local K = {}
+local defaults = {
+  attach_to_current_win = true,
+}
+K.setup = function(cfg)
+  cfg = vim.tbl_extend("keep", cfg or {}, defaults)
+
+  local Term = require "kitty.term"
+  local KT = Term:new(cfg)
+
+  -- Create the illusion of the global singleton, so can use . rather than :
+  setmetatable(K, {
+    __index = function(m, k)
+      local ret = KT[k]
+      if type(ret) == "function" then
+        local f = function(...)
+          return ret(KT, ...)
+        end
+        m[k] = f
+        return f
+      else
+        return ret
+      end
+    end,
+  })
+  return K
+end
+return K
