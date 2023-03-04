08371ed54aa16c12a570d0ecb93827f6026a0d9b
Anshuman <amedhi@connect.ust.hk>
2023-03-04 22:12:40 +0800

    rust_tools.nvim integration

--- lua/kitty/make.lua
+++ lua/kitty/make.lua
@@ -32,71 +32,7 @@ end
 
 -- Append commands to self.targets
 -- name = { cmd = '', desc = ''}
-Make.builtin_target_providers = {
-  ["make"] = function(targets, _)
-    targets.make = {
-      cmd = "make",
-      desc = "Run Makefile",
-      priority = 100,
-    }
-    targets.default = targets.make
-  end,
-  ["just"] = function(targets, _)
-    if not vim.fn.executable "just" then
-      return
-    end
-
-    targets.just_default = {
-      cmd = "just",
-      desc = "Run Justfile default",
-      priority = 100,
-    }
-
-    Make.from_command("just", { "--list" }, function(data)
-      local lines = vim.split(data, "\n")
-      for i, line in ipairs(lines) do
-        if i > 1 then -- Skip first line
-          local parts = vim.split(line, "#")
-          local name = vim.trim(parts[1])
-          local desc = parts[2] and vim.trim(parts[2]) or name
-          targets["just: " .. name] = { cmd = "just " .. name, desc = desc }
-        end
-      end
-    end)
-
-    targets.default = targets.just_default
-  end,
-  ["cargo"] = function(targets)
-    if not vim.fn.filereadable "Cargo.toml" or not vim.fn.executable "cargo" then
-      return
-    end
-
-    targets.check = {
-      cmd = "cargo check",
-      desc = "Check Cargo Project",
-      priority = 100,
-    }
-    targets.build = {
-      cmd = "cargo build",
-      desc = "Build Cargo Project",
-      priority = 99,
-    }
-    targets.test = {
-      cmd = "cargo test",
-      desc = "Test Cargo Project",
-      priority = 98,
-    }
-    targets.run = {
-      cmd = "cargo run",
-      desc = "Run Cargo Project",
-      priority = 97,
-    }
-
-    -- Make.from_command("cargo", {"--", "--list"}, parse)
-
-    targets.default = targets.check
-  end,
-}
+Make.builtin_target_providers = require "kitty.make.builtins"
 
 function Make.setup(T)
   -- Options, state variables, etc
@@ -212,9 +148,8 @@ function Make.setup(T)
     end
 
     cmd = cmd .. "\r"
-    print(cmd)
     if opts.launch_new then
-      self:launch({}, opts.launch_new, { self.shell, "-c", cmd })
+      return self:launch({}, opts.launch_new, { self.shell, "-c", cmd })
     else
       self:send(cmd)
     end
@@ -330,6 +265,15 @@ function Make.setup(T)
     self:call_or_input(name, "_add_target", { prompt = "Name" }, function(i)
       return i, vim.deepcopy(self.targets.last_manual), run_opts
     end)
+  end
+
+  function T:rust_tools_executor(opts)
+    opts = opts or T.default_run_opts
+    return {
+      execute_command = function(command, args, cwd)
+        self:_run("cd " .. cwd .. " && " .. command .. " " .. table.concat(args, " "), opts)
+      end,
+    }
   end
 
   if T.target_providers ~= nil then
