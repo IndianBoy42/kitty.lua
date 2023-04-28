f90cd83d19dac43e031b4794e3f2306adb863b97
Anshuman <amedhi@connect.ust.hk>
2023-04-28 21:54:54 +0800

    Big rework of require'kitty'.setup()
    use the same pipe as the current kitty terminal
    attach to an existing kitty window or create a new one

--- lua/kitty/init.lua
+++ lua/kitty/init.lua
@@ -1,38 +1,91 @@
+-- Get the second window in the same tab as the neovim instance
 local K = {}
-local defaults = {}
-
-K.setup = function(cfg)
+local defaults = {
+  title = "Kitty.nvim",
+  attach_to_existing_os_win = true,
+  attach_to_existing_kt_win = true,
+  attach_to_existing_tab = true,
+}
+K.setup = function(cfg, cb)
+  -- TODO: other_tab
   cfg = vim.tbl_extend("keep", cfg or {}, defaults)
 
-  local KT
-  if cfg.from_current_win then
-    local CW = require("kitty.current_win").setup()
-    KT = CW.sub_window(cfg, cfg.from_current_win)
-  else
-    KT = require("kitty.term"):new(cfg)
+  local CW = require("kitty.current_win").setup()
+  if CW.title == nil then
+    error "kitty.current_win not setup() yet"
+    return
   end
-  K.instance = KT
-
-  -- Create the illusion of the global singleton, so can use . rather than :
-  setmetatable(K, {
-    __index = function(m, k)
-      local ret = KT[k]
-      if type(ret) == "function" then
-        local f = function(...)
-          return ret(KT, ...)
-        end
-        m[k] = f
-        return f
-      else
-        return ret
-      end
-    end,
-  })
 
   K.setup = function(_)
     return K
   end
 
-  return K
+  CW.ls(vim.schedule_wrap(function(data)
+    local KT
+
+    local ls = require("kitty.ls").from_json(data)
+    local found_tab, found_os_win = ls:focused_tab()
+
+    -- Get previous active window in focused tab
+    if not cfg.attach_to_current_win and cfg.attach_to_existing_kt_win then
+      if not found_tab then
+        error "Couldn't find the current window, this is a bug in kitty.current_win"
+      end
+      for i, winid in ipairs(found_tab.active_window_history) do
+        if i > 1 and ls:window_by_id(winid) then
+          cfg.attach_to_current_win = winid
+        end
+      end
+    end
+    -- Find another existing tab in the current OS window
+    if not cfg.attach_to_current_win and cfg.attach_to_existing_tab then
+      for _, tab in ipairs(found_os_win.tabs) do
+        if not tab.is_focused then
+          cfg.attach_to_current_win = tab.active_window_history[1]
+        end
+      end
+    end
+    -- Find another existing OS window
+    if not cfg.attach_to_current_win and cfg.attach_to_existing_os_win then
+      for _, os_win in ipairs(ls.data) do
+        if not os_win.is_focused then
+          for _, tab in ipairs(os_win.tabs) do
+            if tab.is_active then
+              cfg.attach_to_current_win = tab.active_window_history[1]
+            end
+          end
+        end
+      end
+    end
+    -- Couldn't find any existing window
+    if not cfg.attach_to_current_win then
+      vim.notify "Creating other win"
+      KT = CW.launch(cfg, cfg.create_new_win or "window")
+    else
+      vim.notify("Found other win " .. cfg.attach_to_current_win)
+      KT = require("kitty.term"):new(cfg)
+    end
+
+    setmetatable(K, {
+      __index = function(m, k)
+        local ret = KT[k]
+        if type(ret) == "function" then
+          local f = function(...)
+            return ret(KT, ...)
+          end
+          m[k] = f
+          return f
+        else
+          return ret
+        end
+      end,
+    })
+
+    -- K.set_window_title(K.title)
+
+    if cb then
+      cb(K)
+    end
+  end))
 end
 return K
