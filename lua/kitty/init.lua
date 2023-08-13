f1b575ef78f427ae991230e8c9dc1716dca1c094
Anshuman <amedhi@connect.ust.hk>
2023-08-13 16:38:05 +0800

    refactor kitty attach and make it work better with gui nvim

--- lua/kitty/init.lua
+++ lua/kitty/init.lua
@@ -1,11 +1,39 @@
 -- Get the second window in the same tab as the neovim instance
 local K = {}
+local kutils = require "kitty.utils"
 local defaults = {
   title = "Kitty.nvim",
   attach_to_existing_os_win = true,
   attach_to_existing_kt_win = true,
   attach_to_existing_tab = true,
 }
+
+function K.attach_to(cfg, ls)
+  local found = ls:focused_window()
+  -- Get previous active window in focused tab
+  if not cfg.attach_to_current_win and cfg.attach_to_existing_kt_win then
+    for _, winid in ipairs(found.tab.active_window_history) do
+      if winid ~= found.win.id and ls:window_by_id(winid) then cfg.attach_to_current_win = winid end
+    end
+  end
+  -- Find another existing tab in the current OS window
+  if not cfg.attach_to_current_win and cfg.attach_to_existing_tab then
+    for _, tab in ipairs(found.os_win.tabs) do
+      if not tab.is_focused then cfg.attach_to_current_win = tab.active_window_history[1] end
+    end
+  end
+  -- Find another existing OS window
+  if not cfg.attach_to_current_win and cfg.attach_to_existing_os_win then
+    for _, os_win in ipairs(ls.data) do
+      if not os_win.is_focused then
+        for _, tab in ipairs(os_win.tabs) do
+          if tab.is_active then cfg.attach_to_current_win = tab.active_window_history[1] end
+        end
+      end
+    end
+  end
+end
+
 K.setup = function(cfg, cb)
   -- TODO: other_tab
   cfg = vim.tbl_extend("keep", cfg or {}, defaults)
@@ -29,31 +57,17 @@ K.setup = function(cfg, cb)
 
   CW.ls(vim.schedule_wrap(function(data)
     local ls = require("kitty.ls").from_json(data)
-    local found_win, found_tab, found_os_win = ls:focused_window()
+    _G.kitty_initial_ls = ls
+    -- FIXME: this seems unreliable, sometimes we are still in the kitty window
+    local found = ls:focused_os_win()
 
-    -- Get previous active window in focused tab
-    if not cfg.attach_to_current_win and cfg.attach_to_existing_kt_win then
-      if not found_tab then error "Couldn't find the current window, this is a bug in kitty.current_win" end
-      for i, winid in ipairs(found_tab.active_window_history) do
-        if winid ~= found_win.id and ls:window_by_id(winid) then cfg.attach_to_current_win = winid end
-      end
+    if not found then
+      -- TODO: is this the best idea? probably
+      cfg.attach_to_current_win = kutils.current_win_id()
     end
-    -- Find another existing tab in the current OS window
-    if not cfg.attach_to_current_win and cfg.attach_to_existing_tab then
-      for _, tab in ipairs(found_os_win.tabs) do
-        if not tab.is_focused then cfg.attach_to_current_win = tab.active_window_history[1] end
-      end
-    end
-    -- Find another existing OS window
-    if not cfg.attach_to_current_win and cfg.attach_to_existing_os_win then
-      for _, os_win in ipairs(ls.data) do
-        if not os_win.is_focused then
-          for _, tab in ipairs(os_win.tabs) do
-            if tab.is_active then cfg.attach_to_current_win = tab.active_window_history[1] end
-          end
-        end
-      end
-    end
+
+    K.attach_to(cfg, ls)
+
     -- Couldn't find any existing window
     if not cfg.attach_to_current_win then
       vim.notify "Creating a new Kitty window"
--- lua/kitty/ls.lua
+++ lua/kitty/ls.lua
@@ -8,29 +8,56 @@ M.json_to_buffer = vim.schedule_wrap(function(_, raw)
   vim.opt.filetype = "json"
 end)
 
+local function with_cache(tbl, key, fn)
+  if tbl[key] then return tbl[key] end
+  local res = fn(tbl, key)
+  rawset(tbl, key, res)
+  return res
+end
+local function all_windows(self)
+  local tbl = {}
+  for _, tab in pairs(self:all_tabs()) do
+    for _, win in ipairs(tab.windows) do
+      tbl[win.id] = win
+    end
+  end
+  return tbl
+end
+local function all_tabs(self)
+  local tbl = {}
+  for _, os_win in ipairs(self.data) do
+    for _, tab in ipairs(os_win.tabs) do
+      tbl[tab.id] = tab
+    end
+  end
+  return tbl
+end
+local function focused_window(self)
+  local focused = self:focused_tab()
+  if not focused then return nil end
+  local os_win = focused.os_win
+  local tab = focused.tab
+  for _, win in ipairs(tab.windows) do
+    if win.is_focused then return { win = win, tab = tab, os_win = os_win } end
+  end
+end
+local function focused_tab(self)
+  local os_win = self:focused_os_win()
+  if not os_win then return nil end
+  for _, tab in ipairs(os_win.tabs) do
+    if tab.is_focused then return { tab = tab, os_win = os_win } end
+  end
+end
+local function focused_os_win(self)
+  for _, os_win in ipairs(self.data) do
+    if os_win.is_focused then return os_win end
+  end
+end
+
 local methods = {}
-function methods:focused_window()
-  for _, os_win in ipairs(self.data) do
-    if os_win.is_focused then
-      for _, tab in ipairs(os_win.tabs) do
-        if tab.is_focused then
-          for _, win in ipairs(tab.windows) do
-            if win.is_focused then return win, tab, os_win end
-          end
-        end
-      end
-    end
-  end
-end
-function methods:focused_tab()
-  for _, os_win in ipairs(self.data) do
-    if os_win.is_focused then
-      for _, tab in ipairs(os_win.tabs) do
-        if tab.is_focused then return tab, os_win end
-      end
-    end
-  end
-end
+function methods:focused_window() return with_cache(self, "__focused_window", focused_window) end
+function methods:focused_tab() return with_cache(self, "__focused_tab", focused_tab) end
+function methods:focused_os_win() return with_cache(self, "__focused_os_win", focused_os_win) end
 function methods:window_by_id(id)
   if type(id) == "string" then id = tonumber(id) end
   return self:all_windows()[id]
@@ -49,28 +76,8 @@ function methods:tabs()
     if os_win.is_focused then return os_win.tabs end
   end
 end
-function methods:all_windows(tbl)
-  if tbl == nil and self._all_windows_cache then return self._all_windows_cache end
-  tbl = tbl or {}
-  for _, tab in pairs(self:all_tabs()) do
-    for _, win in ipairs(tab.windows) do
-      tbl[win.id] = win
-    end
-  end
-  rawset(self, "_all_windows_cache", tbl)
-  return tbl
-end
-function methods:all_tabs(tbl)
-  if tbl == nil and self._all_tabs_cache then return self._all_tabs_cache end
-  tbl = tbl or {}
-  for _, os_win in ipairs(self.data) do
-    for _, tab in ipairs(os_win.tabs) do
-      tbl[tab.id] = tab
-    end
-  end
-  rawset(self, "_all_tabs_cache", tbl)
-  return tbl
-end
+function methods:all_windows() return with_cache(self, "__all_windows", all_windows) end
+function methods:all_tabs() return with_cache(self, "__all_tabs", all_tabs) end
 
 local metatbl = {
   __index = methods,
@@ -83,4 +90,5 @@ M.term_config = function(ls_win)
     title = ls_win.title,
   }
 end
+
 return M
